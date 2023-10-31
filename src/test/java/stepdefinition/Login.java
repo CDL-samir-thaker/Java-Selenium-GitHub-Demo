944b620ca3fda76a6ed66d383e24b8d241c06d15
Samir.Thaker <samir.thaker@compassdigital.io>
2023-10-30 22:11:29 -0400

    fix(test): run test in headless mode

--- src/test/java/stepdefinition/Login.java
+++ src/test/java/stepdefinition/Login.java
@@ -2,11 +2,14 @@ package stepdefinition;
 import io.cucumber.java.en.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Login {
     @Given("the user is on app")
     public void user_is_on_app(){
-    WebDriver driver = new ChromeDriver();
+        ChromeOptions options= new ChromeOptions();
+        options.addArguments("--headless");
+    WebDriver driver = new ChromeDriver(options);
         // opens the file like real page
         driver.get("https://www.google.com");
     }
